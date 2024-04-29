2df2079e9ac567fd850faacf728b0d683a682d7b
Johan Doris <johndoris213@protonmail.com>
2024-04-30 01:40:33 +0200

    ui: deposit gradients

--- src/app/account/deposits/page.tsx
+++ src/app/account/deposits/page.tsx
@@ -1,6 +1,8 @@
 "use client";
+import EthriumModal2 from "@/components/ethriumModal.ui";
 import Modal from "@/components/modal";
 import BitcoinModal from "@/views/deposit.view/bitcoin.view";
+import EthriumModal from "@/views/deposit.view/ethrium.view";
 import Link from "next/link";
 import { useState } from "react";
 import {
@@ -11,6 +13,7 @@ import {
 } from "react-icons/fa";
 export default function DepositPage() {
   const [toggleBitcoin, setToggleBitcoin] = useState<boolean>(false);
+  const [toggleEthrium, setEthrium] = useState<boolean>(true);
 
   const toggleModalHandler = () => {
     if (toggleBitcoin) {
@@ -19,40 +22,53 @@ export default function DepositPage() {
       return setToggleBitcoin(true);
     }
   };
+
+  const toggleEthriumHandler = () => {
+    if (toggleEthrium) {
+      return setEthrium(false);
+    } else if (!toggleEthrium) {
+      return setEthrium(true);
+    }
+  };
   return (
     <div
       className={
-        " h-screen w-screen overflow-y-scroll flex flex-col gap-y-5 justify-center p-2 items-center"
+        "h-screen w-screen overflow-y-scroll flex flex-col gap-y-5 justify-center p-2 items-center"
       }
     >
       <h1 className="text-2xl font-medium text-gray-400">
         Select Deposit Method.
       </h1>
 
-      <div className="p-2 w-[30vh] bg-gray-300 cursor-pointer flex justify-center shadow-md">
+      <div className="p-4  rounded-lg bg-gradient-to-r from-[#BFBFBF] to-[#a2a2a2] cursor-pointer flex justify-center shadow-md">
         <FaBitcoin
-          style={{ color: "black", fontSize: "128px" }}
+          style={{ color: "black", fontSize: "64px" }}
           onClick={toggleModalHandler}
         />
       </div>
+      <div className="p-4  rounded-lg bg-gradient-to-r from-[#BFBFBF] to-[#a2a2a2] cursor-pointer flex justify-center shadow-md">
+        <FaEthereum style={{ color: "black", fontSize: "64px" }} />
+      </div>
 
-      <div className="p-2 w-[30vh] bg-gray-300 flex justify-center shadow-md">
+      <div className="p-4  rounded-lg bg-gradient-to-r from-[#BFBFBF] to-[#a2a2a2] cursor-pointer flex justify-center shadow-md">
         <Link href={"/account/deposits/mastercard/"}>
-          <FaCcMastercard style={{ color: "black", fontSize: "128px" }} />
+          <FaCcMastercard style={{ color: "black", fontSize: "64px" }} />
         </Link>
       </div>
-      <div className="p-2 w-[30vh] bg-gray-300 flex justify-center shadow-md">
+      <div className="p-4  rounded-lg bg-gradient-to-r from-[#BFBFBF] to-[#a2a2a2] cursor-pointer flex justify-center shadow-md">
         <Link href={"/account/deposits/visa/"}>
-          <FaCcVisa style={{ color: "black", fontSize: "128px" }} />
+          <FaCcVisa style={{ color: "black", fontSize: "64px" }} />
         </Link>
-      </div>
-      <div className="p-2 w-[30vh] bg-gray-300 flex justify-center shadow-md">
-        <FaEthereum style={{ color: "black", fontSize: "128px" }} />
       </div>
       {toggleBitcoin ? (
         <Modal onClick={toggleModalHandler}>
           <BitcoinModal />
         </Modal>
+      ) : null}
+      {toggleEthrium ? (
+        <EthriumModal2 onClick={toggleEthriumHandler}>
+          <EthriumModal />
+        </EthriumModal2>
       ) : null}
     </div>
   );
