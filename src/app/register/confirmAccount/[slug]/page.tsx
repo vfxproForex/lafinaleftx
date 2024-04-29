5a26f3a07c34215c33e0d38fab31dd56e01da2ed
Johan Doris <johndoris213@protonmail.com>
2024-04-29 21:42:18 +0200

    fix: signin page

--- src/app/register/confirmAccount/[slug]/page.tsx
+++ src/app/register/confirmAccount/[slug]/page.tsx
@@ -11,43 +11,29 @@ export default function ConfirmAccountPage({
   const confirmEmailHandler = async () => {
     const requestBody = {
       query: `
-mutation {
-  confirmEmail(id: "${params.slug}")
-}
+        mutation {
+            confirmEmail(id: "${params.slug}")
+        }
 `,
     };
 
-    await toast.promise(
-      fetch("http://localhost:3000/graphql", {
-        method: "POST",
-        body: JSON.stringify(requestBody),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => {
-          if (res.status !== 200 && res.status !== 201) {
-            console.log(res.json());
-            return console.log("Try Logging in..");
-          }
-          return res.json();
-        })
-        .then(async (data) => {
-          if (data.error) {
-            return console.log(`Error: ${data.errors.errors.message}`);
-          }
-          //console.log(data.data.register.user.email);
-          //await data.data.register.user.email;
-          await data;
-          return route.push("/signin");
-        }),
+    const response = await fetch("http://localhost:3000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-      {
+    return await toast
+      .promise(response.json(), {
         loading: "Loading",
-        success: "E-Mail confirm, please login.",
-        error: `Please contact support`,
-      }
-    );
+        success: "Welcome To Pinnacle FTX Fellow Trader.",
+        error: "Please contact support, unable to confirm email.",
+      })
+      .then(() => {
+        return route.push("/signin");
+      });
   };
 
   return (
@@ -62,7 +48,6 @@ mutation {
       >
         Cofirm E-Mail
       </button>
-      <h1>Small test: {params.slug}</h1>
       <Toaster />
     </div>
   );
