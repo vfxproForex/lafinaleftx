eb9dab1bfcf550a4b945fd638e60b08bf820fe40
Johan Doris <johndoris213@protonmail.com>
2024-04-30 04:55:07 +0200

    server test

--- src/app/register/confirmAccount/[slug]/page.tsx
+++ src/app/register/confirmAccount/[slug]/page.tsx
@@ -19,7 +19,7 @@ export default function ConfirmAccountPage({
 `,
       };
 
-      const response = await fetch("http://localhost:3000/graphql", {
+      const response = await fetch("http://127.0.0.1:3000/graphql", {
         method: "post",
         body: JSON.stringify(requestBody),
         headers: {
