cc6af32f1b8315bcfe811c178e3f4475e6802898
Johan Doris <johndoris213@protonmail.com>
2024-05-02 04:59:11 +0200

    ui: submit button

--- src/components/submitButton.tsx
+++ src/components/submitButton.tsx
@@ -1,8 +1,7 @@
 "use client";
-
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { useFormStatus } from "react-dom";
-import { Toaster, toast } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 interface IProps {
   buttonCaption: string;
@@ -11,25 +10,14 @@ interface IProps {
 const SubmitButton: FC<IProps> = (props) => {
   const { pending } = useFormStatus();
 
-  //useEffect(() => {
-  //if(!pending){
-  //toast.promise('Registering', {
-  //duration: 4000,
-  //position: 'top-center',
-  //},
-  //loading: 'Loading',
-  //success: 'Got the data',
-  //error: 'Error when fetching',
-  //)
-  //}
-  //}, []);
-
   return (
     <button
       aria-disabled={pending}
       className={`p-2 ${
-        pending ? "bg-gray-300" : "bg-gray-500"
-      }  w-[20vh] mb-6 rounded-md`}
+        pending
+          ? "bg-cs_primary-300 text-cs_primary-300"
+          : "text-cs_primary-100 bg-cs_accent-200"
+      }  w-[20vh] mb-6 rounded-md font-semibold `}
       type="submit"
     >
       {!pending ? props.buttonCaption : props.loadingTitle}
